d173bebbc27af90372764a59fa7538dd153c2cdf
Byron <zhion360@gmail.com>
2014-06-27 07:58:20 +0200

    Create Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+{
+  "name": "TypeShift",
+  "version": "0.1.0",
+  "devDependencies": {
+    "grunt": "~0.4.1",
+    "grunt-contrib-concat": "^0.3.0",
+    "grunt-contrib-sass": "^0.7.3",
+    "grunt-contrib-watch": "^0.6.1",
+    "grunt-contrib-haml": "^0.1.2",
+    "grunt-combine-media-queries": "^1.0.15"
+  }
+}
