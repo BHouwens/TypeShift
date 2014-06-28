89a62b13d70f6b172d310257d02066addfa1dbdb
Byron <zhion360@gmail.com>
2014-06-28 18:09:55 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,8 @@ module.exports = function(grunt){
                 },
                 
                 files: {
-<<<<<<< HEAD
-                    'stylesheets/main.css': 'stylesheets/sass/main.scss'
-=======
-                    'stylesheets/main.css': 'stylesheets/sass/**/*.scss'
->>>>>>> 4b7481f2abba2ed3ea1279031603b31467d5eba9
+                  'stylesheets/main.css': 'stylesheets/sass/main.scss'
+
                 }
             }
         },
@@ -37,32 +34,19 @@ module.exports = function(grunt){
             },
         
             css:{
-<<<<<<< HEAD
                 files: ['stylesheets/sass/main.scss'],
-=======
-                files: ['stylesheets/sass/**/*.scss'],
->>>>>>> 4b7481f2abba2ed3ea1279031603b31467d5eba9
                 tasks: ['sass'],
                 options:{
                     spawn: false,
                 }
             },
-<<<<<<< HEAD
 
-=======
-            
->>>>>>> 4b7481f2abba2ed3ea1279031603b31467d5eba9
             cmq:{
                 files:['stylesheets/*.css'],
                 tasks: ['cmq'],
                 options:{
                     spawn: false,
                 }
-<<<<<<< HEAD
-            },
-=======
-            }
->>>>>>> 4b7481f2abba2ed3ea1279031603b31467d5eba9
         }
         
     });
@@ -71,10 +55,5 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-combine-media-queries');
 
-<<<<<<< HEAD
     grunt.registerTask('default', ['sass','cmq']);
-=======
-    grunt.registerTask('default', ['sass', 'cmq']);
->>>>>>> 4b7481f2abba2ed3ea1279031603b31467d5eba9
-    
 };
