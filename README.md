71e92154e2ed7e40501fc95bfa7b22da23e9ed60
Byron <zhion360@gmail.com>
2014-07-02 21:04:55 +0200

    Batch 1

---
+++ README.md
@@ -0,0 +1,4 @@
+TypeShift
+=========
+
+TypeShift is a single page app that automagically converts into a range of typographic units for you on the fly. It's built on AngularJS and makes figuring out different unit values quick and painless.
---
+++ js/main.js
@@ -0,0 +1,79 @@
+var TypeShift = angular.module('TypeShift', []);
+
+TypeShift.controller('MainController', ['$scope', function($scope){
+	$scope.options = {
+		type: "select",
+		name: "px",
+		value: "px",
+		values: ["px", "points", "ems", "%"]
+	}
+	
+	$scope.units = ["px", "pts", "ems", "%"];
+	
+	$scope.calculate = function(value){
+			if ($("option:selected").text() == "px"){
+				switch(value){
+					case "px":
+						return $scope.input;
+						break;
+					case "pts":
+				 		return ($scope.input / 1.3333);
+						break;
+					case "ems":
+						return ($scope.input / 16);
+						break;
+					case "%":
+						return ($scope.input / 16 * 100);
+						break;
+				}
+			}else
+			if  ($("option:selected").text() == "points"){
+				switch(value){
+					case "px":
+						return ($scope.input * 1.3333);
+						break;
+					case "pts":
+				 		return $scope.input;
+						break;
+					case "ems":
+						return ($scope.input / 12);
+						break;
+					case "%":
+						return ($scope.input / 12 * 100);
+						break;
+				}
+			}else
+			if ($("option:selected").text() == "ems"){
+				switch(value){
+					case "px":
+						return ($scope.input * 16);
+						break;
+					case "pts":
+				 		return ($scope.input  * 12);
+						break;
+					case "ems":
+						return $scope.input;
+						break;
+					case "%":
+						return ($scope.input  * 100);
+						break;
+				}
+			}else
+			if ($("option:selected").text() == "%"){
+				switch(value){
+					case "px":
+						return ($scope.input * 16 / 100);
+						break;
+					case "pts":
+				 		return ($scope.input  * 12 / 100);
+						break;
+					case "ems":
+						return ($scope.input / 100);
+						break;
+					case "%":
+						return $scope.input;
+						break;
+				}
+			}
+	}//end $scope.calculate
+}]);
